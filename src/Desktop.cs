95e01d73708eccbbcd810fd2d98b55ea25210f3d
Robin van Nunen <robin@vnunen.nl>
2016-08-27 18:10:07 +0200

    Update wallpaper with random image

---
+++ src/Desktop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace BackgroundChanger
+{
+    class Desktop
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        private const int SPI_SETDESKWALLPAPER = 20;
+        private const int SPIF_UPDATEINIFILE = 0x1;
+        private const int SPIF_SENDWININICHANGE = 0x2;
+
+        public static readonly string SourceUrl = "https://source.unsplash.com/random/1920x1080";
+        public static readonly string DestinationFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "background.jpg");
+
+
+        /// <summary>
+        /// Update desktop wallpaper with new image
+        /// </summary>
+        public static void Update()
+        {
+            Downloader.DownloadImageToFile(SourceUrl, DestinationFile);
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, DestinationFile, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.IO;
-
+﻿
 namespace BackgroundChanger
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string dest = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cat.jpg");
-            Downloader.DownloadImageToFile("https://c2.staticflickr.com/1/70/175237265_029f7974a2_b.jpg", dest);
+            Desktop.Update();
         }
     }
 }
